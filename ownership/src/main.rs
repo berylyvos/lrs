f16d1a25f84a587ba2bbc2894a130bf08d05eb68
Shingo Gnix <870656738@qq.com>
2023-03-29 19:13:26 +0800

    feat: references & borrowing

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -28,9 +28,68 @@ fn copy_i32() {
     println!("px = {:?}, py = {:?}", px, py);
 }
 
+fn unmutable_reference() {
+    let s1 = String::from("hello");
+    let s2 = &s1;
+    let len = calculate_length(s2);
+
+    println!("s1: {:?}, s2: {:?}", s1.as_ptr(), s2.as_ptr());
+    println!("The length of '{}' is {}.", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    println!("s: {:?}", s.as_ptr());
+    s.len()
+}
+
+fn mutable_reference() {
+    let mut s = String::from("hello");
+
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", shingo");
+}
+
+fn mutable_and_unmutable() {
+   let mut s = String::from("SHINGO");
+
+    let r1 = &s;
+    let r2 = &s;
+    println!("{} and {}", r1, r2);
+    // 新编译器中，r1,r2作用域在这里结束
+
+    let r3 = &mut s;
+    println!("{}", r3);
+} // 老编译器中，r1、r2、r3作用域在这里结束
+  // 新编译器中，r3作用域在这里结束
+
+fn dangling_reference() {
+    let _s = no_dangle();
+}
+
+/* 
+fn dangle() -> &String {
+    let s = String::from("hello");
+// this function's return type contains a borrowed value, but there is no value for it to be borrowed from
+// help: consider using the `'static` lifetime
+    &s
+}
+*/
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+    s
+}
+
 fn main() {
     ownership_move();
     copy();
     clone();
     copy_i32();
+    unmutable_reference();
+    mutable_reference();
+    mutable_and_unmutable();
+    dangling_reference();
 }
